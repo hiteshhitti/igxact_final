7464544188d9ee088d13f5a23139c2731c463570
hiteshhitti <hitesh.hitti@gmail.com>
2026-04-23 18:06:08 +0530

    datasdc

--- backend/main.py
+++ backend/main.py
@@ -475,6 +475,13 @@ def get_data(year: int = Query(None),
         sheet=client.open_by_url("https://docs.google.com/spreadsheets/d/11SVXk8gh1RRwS7U-rvxfnYx_ieIrqoyAavmkFWwMHjA/edit?gid=0#gid=0").sheet1
         data=sheet.get_all_records()
         df = pd.DataFrame(data)
+        df.columns = (
+            df.columns
+            .str.strip()
+            .str.replace('\n', ' ')
+            .str.replace('\r', ' ')
+            .str.replace(r'\s+', ' ', regex=True)
+        )
         df = df.replace('', np.nan)   # turn all empty strings into NaN
         
 
